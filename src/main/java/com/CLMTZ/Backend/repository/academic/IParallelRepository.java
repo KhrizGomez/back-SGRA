55a3e4df2f258888c95100ae2ca24648c5a1a1a7
Melanir <mmunozv6@uteq.edu.ec>
2026-03-14 13:39:09 -0500

    Agregar métodos para actualizar detalles de matrícula y paralelos existentes en el servicio de matrícula

--- src/main/java/com/CLMTZ/Backend/repository/academic/IParallelRepository.java
+++ src/main/java/com/CLMTZ/Backend/repository/academic/IParallelRepository.java
@@ -1,9 +1,13 @@
 package com.CLMTZ.Backend.repository.academic;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.CLMTZ.Backend.model.academic.Parallel;
 
 public interface IParallelRepository extends JpaRepository<Parallel, Integer> {
+
+    Optional<Parallel> findBySectionIgnoreCase(String section);
 
 }
